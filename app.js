71ecb1504cd8848b01e7c0fee30d176f97c9a1a6
madghigno <madghigno@gmail.com>
2018-10-05 19:29:08 +0200

    fourth project commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-var port = process.env.port|| 3000
+var port = process.env.PORT|| 3000
 app.get('/', (req, res) => res.send('Hello Wolrd !!!'))
 
 app.listen(port, () => console.log('Server is running on port: ' + port))
